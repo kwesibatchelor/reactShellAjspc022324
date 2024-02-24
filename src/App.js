97e916179f8ae6f08314efedcb6b7e523753cf1c
kwesibatchelor <kwesibatchelor@gmail.com>
2024-02-23 22:30:02 -0500

    react shell 0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Kwesi</h1>
+      <p>
+        <Link to="/ccc" className="link">CCC</Link>
+        <br />
+        <br />
+        <Link to="/dev" className="link">Dev</Link>
+      </p>
+    </div>
+  );
+};
+
+const CccPage = () => {
+  return <div><h1>Example from CCC</h1></div>
+  //return <h1>CCC</h1>;
+};
+
+const DevPage = () => {
+  return <div><h1>Example from Dev</h1></div>
+  //return <h1>Dev</h1>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center"> 
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ccc" element={<CccPage />} />
+          <Route path="/dev" element={<DevPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+/*
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return <h1>Welcome to Kwesi's Home Page</h1>;
+}
+
+const KwesiPage = () => {
+  return (
+    <div>
+      <h1>Kwesi</h1>
+      <p>
+        <Link to="/kwesi/khari">Khari</Link>
+        <br />
+        <Link to="/kwesi/batchelor">Batchelor</Link>
+      </p>
+    </div>
+  );
+};
+
+const KhariPage = () => {
+  return <h1>Khari</h1>;
+};
+
+const BatchelorPage = () => {
+  return <h1>Batchelor</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/kwesi" element={<KwesiPage />} />
+        <Route path="/kwesi/khari" element={<KhariPage />} />
+        <Route path="/kwesi/batchelor" element={<BatchelorPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+*/
